69f2c451665682213948f6c8beb0b4294df60163
William Tessari <william@MacBook-Air-de-William.local>
2023-07-02 23:41:55 +0200

    clean syntaxe

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-const initUser = async() => {
-    const newUser = await prisma.users.create({
-        data: {
-          password: 'ijduidhgzuey',
-          email: 'leo@gmail.io',
-        },
-      }).then((t) => console.log(t))
-}
+const initUser = async () => {
+  const newUser = await prisma.users
+    .create({
+      data: {
+        password: "ijduidhgzuey",
+        email: "leo@gmail.io",
+      },
+    })
+    .then((t) => console.log(t));
+};
 
-initUser()
- 
+initUser();
