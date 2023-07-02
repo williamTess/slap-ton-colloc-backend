9b0aa05907154d8618b14a0c249dc221ac33e58c
William Tessari <william@MacBook-Air-de-William.local>
2023-07-02 23:40:15 +0200

    added prettier

--- src/pages/api/users/[id].ts
+++ src/pages/api/users/[id].ts
@@ -2,17 +2,17 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../utils/prisma";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const {id: idNotChecked} = req.query
-    const id = Array.isArray(idNotChecked) ? idNotChecked[0]: idNotChecked
-    
-    if (!id) {
-    return res.status(400).json({err: "Not a valid ID"})
-}
-    
-const user = await prisma.users.findUnique({where: {id:id}})
-if (!user) return res.status(404).json({err: "User not found"})
+  const { id: idNotChecked } = req.query;
+  const id = Array.isArray(idNotChecked) ? idNotChecked[0] : idNotChecked;
 
-return res.status(200).json(user)
-}
+  if (!id) {
+    return res.status(400).json({ err: "Not a valid ID" });
+  }
+
+  const user = await prisma.users.findUnique({ where: { id: id } });
+  if (!user) return res.status(404).json({ err: "User not found" });
+
+  return res.status(200).json(user);
+};
 
 export default handler;
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -2,10 +2,9 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../utils/prisma";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const users = await prisma.users.findMany()
-    
-    return res.status(200).json(users)
+  const users = await prisma.users.findMany();
 
-}
+  return res.status(200).json(users);
+};
 
 export default handler;
