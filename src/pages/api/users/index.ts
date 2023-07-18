ddcee12ab8a8289ed036b704679dfb23bc1e254b
William Tessari <william@MacBook-Air-de-William.local>
2023-07-18 23:17:06 +0200

    test

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -2,9 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../utils/prisma";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const users = await prisma.users.findMany();
+  if (req.method === "GET") {
+    const users = await prisma.users.findMany();
 
-  return res.status(200).json(users);
+    return res.status(200).json(users);
+  }
+  if (req.method === "POST") {
+    console.log(req.body, req.headers, req.query);
+
+    return res.status(200).json({ test: "test" });
+  }
 };
 
 export default handler;
