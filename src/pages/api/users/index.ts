1d1f9ddc97ffea02c1236aabac23386444e5a43e
William Tessari <william@MacBook-Air-de-William.local>
2023-07-18 23:59:00 +0200

    added remove user

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -9,31 +9,39 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   if (req.method === "POST") {
-    const data = req.body.data;
-    const user = await prisma.users.findUnique({ where: { clerkId: data.id } });
-
-    if (user) return res.status(401).json({ error: "User already created" });
-
-    await prisma.users.create({
-      data: {
-        clerkId: data.id,
-        email: data.email_addresses[0].email_address,
-      },
-    });
-
-    return res.status(200).json({ message: "user Successfully created" });
+    return postUsers(req, res);
   }
 
   if (req.method === "DELETE") {
-    const data = req.body.data;
-    const user = await prisma.users.findUnique({ where: { clerkId: data.id } });
-
-    if (!user) return res.status(401).json({ error: "User doesn't exist" });
-
-    await prisma.users.delete({ where: { clerkId: user.clerkId } });
-
-    return res.status(200).json({ message: "user Successfully created" });
+    return deleteUsers(req, res);
   }
+};
+
+const postUsers = async (req: NextApiRequest, res: NextApiResponse) => {
+  const data = req.body.data;
+  const user = await prisma.users.findUnique({ where: { clerkId: data.id } });
+
+  if (user) return res.status(401).json({ error: "User already created" });
+
+  await prisma.users.create({
+    data: {
+      clerkId: data.id,
+      email: data.email_addresses[0].email_address,
+    },
+  });
+
+  return res.status(200).json({ message: "user Successfully created" });
+};
+
+const deleteUsers = async (req: NextApiRequest, res: NextApiResponse) => {
+  const data = req.body.data;
+  const user = await prisma.users.findUnique({ where: { clerkId: data.id } });
+
+  if (!user) return res.status(401).json({ error: "User doesn't exist" });
+
+  await prisma.users.delete({ where: { clerkId: user.clerkId } });
+
+  return res.status(200).json({ message: "user Successfully created" });
 };
 
 export default handler;
