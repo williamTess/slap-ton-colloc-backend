4b79172e309f75ef95d48a3034fcb3b0a664aedb
William Tessari <william@MacBook-Air-de-William.local>
2023-07-18 23:31:46 +0200

    add users

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -9,8 +9,21 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   }
   if (req.method === "POST") {
     console.log(req.body, req.headers, req.query);
+    const data = req.body.data;
+    const user = await prisma.users.findUnique({ where: { id: data.id } });
 
-    return res.status(200).json({ test: "test" });
+    if (user) return res.status(401).json({ error: "User already created" });
+
+    const newUser = await prisma.users
+      .create({
+        data: {
+          clerkId: data.id,
+          email: data.email_addresses[0].email_address,
+        },
+      })
+      .then((t) => console.log(t));
+
+    return res.status(200).json({ message: "user Successfully created" });
   }
 };
 
