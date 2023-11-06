e6dd45933e8b8d99db8fa98196d0a662d159db10
William Tessari <william@MacBook-Air-de-William.local>
2023-11-06 15:22:18 +0100

    posters added

---
+++ src/pages/api/posters/index.ts
@@ -0,0 +1,15 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../../utils/prisma";
+import data from "../../../../prisma/infos.json";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "GET") {
+    const posters = await prisma.posters.findMany();
+    const posterfilters = data.filterPoster;
+
+    return res.status(200).json({ filters: posterfilters, posters: posters });
+  }
+  return res.status(404).json("error: page not found");
+};
+
+export default handler;
