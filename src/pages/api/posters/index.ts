defef2c76aef61e7414fc291a2889b66b7ec8007
William Tessari <william@MacBook-Air-de-William.local>
2023-11-19 17:45:45 +0100

    console

--- src/pages/api/posters/index.ts
+++ src/pages/api/posters/index.ts
@@ -5,6 +5,7 @@ import data from "../../../../prisma/infos.json";
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     const posters = await prisma.posters.findMany();
+    console.log(posters);
     const posterfilters = data.filterPoster;
 
     return res.status(200).json({ filters: posterfilters, posters: posters });
